4e6d649528db2f903d38298f19b0a84180e745c1
Matt Fisher <mfisher87@gmail.com>
2025-10-23 12:52:07 -0600

    Support layout restoration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import {
+  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { ICommandPalette } from '@jupyterlab/apputils';
+import {
+  ICommandPalette,
+  WidgetTracker
+} from '@jupyterlab/apputils';
 import { ILauncher } from '@jupyterlab/launcher';
 import { imageIcon } from '@jupyterlab/ui-components';
 
@@ -17,12 +21,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension that displays a random image and caption.',
   autoStart: true,
   requires: [ICommandPalette, ILauncher],  // dependencies of our extension
+  optional: [ILayoutRestorer],
   activate: (
     app: JupyterFrontEnd,
     // The activation method receives dependencies in the order they are specified in
     // the "requires" parameter above:
     palette: ICommandPalette,
-    launcher: ILauncher
+    launcher: ILauncher,
+    restorer: ILayoutRestorer | null
   ) => {
     console.log('JupyterLab extension jupytercon2025-extension-workshop is activated!');
 
@@ -36,12 +42,22 @@ const plugin: JupyterFrontEndPlugin<void> = {
         );
       });
 
+    // Track widget state
+    const tracker_namespace = 'jupytercon2025-extension-workshop';
+    const tracker = new WidgetTracker<ImageCaptionMainAreaWidget>({
+      namespace: tracker_namespace
+    });
+
     //Register a new command:
     const command_id = 'image-caption:open';
     app.commands.addCommand(command_id, {
       execute: () => {
         // When the command is executed, create a new instance of our widget
         const widget = new ImageCaptionMainAreaWidget();
+
+        if (!tracker.has(widget)) {
+          tracker.add(widget);
+        }
 
         // Then add it to the main area:
         app.shell.add(widget, 'main');
@@ -52,6 +68,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
     palette.addItem({ command: command_id, category: 'Tutorial' });
     launcher.add({ command: command_id });
+
+    // Restore widget state
+    if (restorer) {
+      restorer.restore(tracker, {
+        command: command_id,
+        name: () => tracker_namespace
+      });
+    }
   }
 };
 
